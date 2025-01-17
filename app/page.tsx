2595e1c0075f16d757776a1b9c6c87488904c6bd
Michel Loos <mloos@mloos.eti.br>
2025-01-17 09:39:15 -0300

    Participantes mais recentes primeiros

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 ">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+        <h1 className="text-4xl"> Bem vindo à Sinfonia </h1>
+      </main>
+
+    </div>
+  );
+}
