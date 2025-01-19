08ef9b0a2d9cb3443105b2dbe9c86816147a45dc
Michel Loos <mloos@mloos.eti.br>
2025-01-19 10:34:27 -0300

    createParticipante

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,6 +12,7 @@ import { AuthError } from 'next-auth';
 import { signIn } from '@/auth';
 */}
 
+{/* Usuarios */}
 
 const UserFormSchema = z.object({
 	id: z.string(),
@@ -35,12 +36,7 @@ const UserFormSchema = z.object({
 });
 
 
-{/*
-const ParticipanteListSchema = z.object({
-	id: z.number(),
-	name: z.string().min(3,"Nome deve ter no minimo 3 letras").max(50,"Nome deve ter no maximo 50 letras"),
-})
-	*/}
+
 	
 
 
@@ -162,6 +158,78 @@ export async function authenticate(
 }
 */}
 
+{/* Participantes */}
+
+
+const ParticipanteFormSchema = z.object({
+	id: z.number(),
+	nome: z.string().min(3,"Nome deve ter no minimo 3 letras").max(100,"Nome deve ter no maximo 100 letras"),
+})
+
+export type ParticipanteState = {
+	errors?: {
+		nome?: string[];
+	};
+	message?: string | null;
+}
+
+const CreateParticipante = ParticipanteFormSchema.omit({id: true})
+
+export async function createParticipante (prevState: ParticipanteState, formData:FormData)
+{
+	mylog("DBG","app/lib/actions","createParticipantes","formdata=",formData);
+	const validatedFields = CreateParticipante.safeParse({
+		nome: formData.get('nome'),
+	});
+	
+	mylog ("DBG", "app/lib/actions", "createParticipante", "validatedFields=",validatedFields);
+
+	if(!validatedFields.success) {
+		mylog ("ERROR", "app/lib/actions", "createParticipante", "validatedFields=", validatedFields.error.flatten().fieldErrors);
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields, failed to create',
+		}
+	}
+
+	const nome = validatedFields.data.nome;
+	mylog ("DBG", "app/lib/actions", "createParticipante", "nome=",nome);
+
+	try {
+		const myreq = `
+		INSERT INTO REUNIAO_T4000_Participantes
+		(Nm_Participante)
+		VALUES ('${nome}')
+		`;
+		const answer = await mssql(myreq);
+		mylog ("DBG", "app/lib/actions", "createParticipante", "answer=",answer);
+	} catch (error) {
+		mylog ("ERROR", "app/lib/actions", "createParticipante", "error=",error);
+		return {
+			message: 'Database Error: Nao criou Participante'
+		};
+	};
+
+	redirect('/sinfonia/administracao/participantes');
+
+}
+
+
+export async function participantes (id: string)
+{
+	mylog ("DBG", "app/lib/actions", "participantes", "id=",id);
+	const goto =  "/sinfonia/reuniao/participantes/"+id+"/edit";
+	redirect (goto);
+}
+export async function escParticipant (id: string)
+{
+	mylog ("DBG", "app/lib/actions", "participantes", "id=",id);
+	const goto =  "/sinfonia/reuniao/participantes/"+id+"/edit";
+	redirect (goto);
+}
+
+
+{/* Reunioes */}
 
 export async function deleteReuniao (id: string)
 {
@@ -182,19 +250,6 @@ export async function deleteReuniao (id: string)
 	}
        */}
 	redirect ('/proto');
-}
-
-export async function participantes (id: string)
-{
-	mylog ("DBG", "app/lib/actions", "participantes", "id=",id);
-	const goto =  "/sinfonia/reuniao/participantes/"+id+"/edit";
-	redirect (goto);
-}
-export async function escParticipant (id: string)
-{
-	mylog ("DBG", "app/lib/actions", "participantes", "id=",id);
-	const goto =  "/sinfonia/reuniao/participantes/"+id+"/edit";
-	redirect (goto);
 }
 
 export type ReuniaoState = {
